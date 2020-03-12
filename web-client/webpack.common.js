87722cfa54f1265d83c42acac9c392bbf889fd5f
Ross Basarevych <basarevych@gmail.com>
2020-03-12 17:55:49 +0200

    Disable preact

--- web-client/webpack.common.js
+++ web-client/webpack.common.js
@@ -15,10 +15,12 @@ module.exports = {
   },
   resolve: {
     extensions: ["*", ".js", ".jsx"],
+    /*
     alias: {
       react: "preact/compat",
       "react-dom": "preact/compat",
     },
+    */
   },
   module: {
     rules: [
